77635a9fdcc00e71bc683b425d030406e25e1bfb
Delphine Gouwy <delphine.gouwy@gmail.com>
2014-04-08 11:29:14 +0200

    Ajout de l'application WPF et realisation de la classe Film

--- leProjet/Cinema/Film.cs
+++ leProjet/Cinema/Film.cs
@@ -7,6 +7,28 @@ namespace Cinema
 {
     class Film
     {
-
+        private string titre;
+        private string resume;
+        public Film(string unTitre, string unResume)
+        {
+            this.titre = unTitre;
+            this.resume = unResume;
+        }
+        public string getTitre()
+        {
+            return this.titre;
+        }
+        public string getResume()
+        {
+            return this.resume;
+        }
+        public void setTitre(string unTitre)
+        {
+            this.titre = unTitre;
+        }
+        public void setResume(string unResume)
+        {
+            this.resume = unResume;
+        }
     }
 }
